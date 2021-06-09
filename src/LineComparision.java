29f9fc4037ce397cce4ac374d5a7dd055131e320
adarshEr2020 <adarshprajapati31@gmail.com>
2021-06-09 13:25:46 +0530

    calculte two line length

--- src/LineComparision.java
+++ src/LineComparision.java
@@ -2,13 +2,26 @@
 public class LineComparision {
 
 	public static void main(String[] args) {
-		
 		System.out.println("Welcome to Line  Comparision computation");
-		// variable
-		double x1=1, x2=2, y1=4, y2=2, lengthOfLine;
-		// calculation
-		lengthOfLine = Math.sqrt( (Math.pow((x2-x1),2))  + Math.pow((y2-y1),2) );
-		System.out.println("length of line :" +lengthOfLine);
+		// line 1 computation
+		int x1, x2, y1, y2;
+		x1=2; x2=5; y1=6; y2=9;
+		
+		// Simple way to calculate
+		double length1 = Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
+		
+		// OR using Pow method
+		//double length = Math.sqrt(Math.pow(x2-x1,2) + Math.pow(y2-y1,2));
+		System.out.println("Length of line :" + length1);
+		
+		// line 2 computation
+		int p1=3, p2=4, q1=6, q2=4;
+		double length2 = Math.sqrt((p2-p1)*(p2-p1) + (q2-q1)*(q2-q1));
+		System.out.println("Length of line :" + length2);
+		if (length1 == length2)
+			System.out.println("line is Equal");
+		else
+			System.out.println("line is Not Equal");
 	}
 
 }
